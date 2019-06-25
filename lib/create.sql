369a26fb747d5b7a526676f4f37a460e12dec619
Renee Nordholm <reneenordholm@gmail.com>
2019-06-25 22:01:45 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,20 @@
+CREATE TABLE project (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date INTEGER,
+  end_date INTEGER
+);
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledge (
+  amount INTEGER,
+  project_id INTEGER,
+  user_id INTEGER
+);
